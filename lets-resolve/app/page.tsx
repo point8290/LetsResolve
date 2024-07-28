24f2a1cef4b04dcacbc3cb05a186bcee995bd99b
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-07-28 16:53:20 +0530

    metadata updatedd

--- lets-resolve/app/page.tsx
+++ lets-resolve/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <h1> Hello there</h1>
+      <div>
+        <p className=" font-bold">
+          Customer query ticketing and AI chatbot application
+        </p>
+      </div>
     </main>
   );
 }
